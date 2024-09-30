b8cece0b467c2f3725276d14ea902e37b9d5cfd9
Michael Wulff Nielsen <mwulff@gmail.com>
2024-09-30 11:37:26 +0200

    Requirements cleanup

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -1,6 +1,1 @@
-from .jwt import create_access_token as create_access_token
-from .jwt import create_refresh_token as create_refresh_token
-from .jwt import verify_password as verify_password
-from .jwt import get_password_hash as get_password_hash
-
 from .oauth2 import oauth2_scheme as oauth2_scheme
