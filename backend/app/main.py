2e91cc46de23e491adfedb6631cfd1001ce8cc43
Michael Wulff Nielsen <mwulff@gmail.com>
2024-10-28 11:17:21 +0100

    Added a little startup logging to see environment

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,16 +18,19 @@ from app.api.v1.accesstoken_router import router as v1_accesstoken_router
 from app.api.token_router import router as token_router
 
 from app.database.session import create_db_and_tables
-
+from app.config import settings
 from app.scheduler import schedule
 
 logging.basicConfig(level=logging.INFO)
 
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     asyncio.create_task(background_task())
+
+    logger.info(f"Starting up, database url is: {settings.database_url}, debug is {settings.debug}")
 
     yield
 
