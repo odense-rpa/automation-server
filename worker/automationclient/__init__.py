fc9eacd3b6f0f5ea63aee3e6c0a4bb1d1a029426
Michael Wulff Nielsen <mwulff@gmail.com>
2024-11-19 09:20:02 +0100

    Fixed url after endpoint move

--- worker/automationclient/__init__.py
+++ worker/automationclient/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-automationserver_url = os.getenv('ATS_URL') or "http://localhost:8000/api"
+automationserver_url = os.getenv('ATS_URL') or "http://localhost:8000"
 automationserver_token = os.getenv('ATS_TOKEN') or ""
 
 headers = { "Authorization": f"Bearer {automationserver_token}" } if automationserver_token else {}
