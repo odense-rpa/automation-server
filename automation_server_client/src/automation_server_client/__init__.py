bfec4bd1362e7546b0aad64184b2105a4d708aa5
Michael Wulff Nielsen <mwulff@gmail.com>
2025-02-26 16:02:43 +0100

    Removed automation_server_client and moved it to a seperate repository on github. This enables package install

--- automation_server_client/src/automation_server_client/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from ._config import AutomationServerConfig
-from ._server import AutomationServer
-from ._models import Session, Process, Workqueue
-
-__all__ = ['AutomationServerConfig', 'AutomationServer', 'Session', 'Process', 'Workqueue']
