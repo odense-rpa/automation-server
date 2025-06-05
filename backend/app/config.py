26bac80d50df9488e467f894b33119a341280676
Michael Wulff Nielsen <mwulff@gmail.com>
2025-06-05 22:01:38 +0200

    Included config example in backend and added .env to gitignore

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     debug: bool = False
-    database_url: str = "sqlite:///automation.db"
+    database_url: str = "postgresql://localhost:5432/ats"
     encryption_key: str = "set me in the env file"
     password_salt: str = "set me in the env file"
-
+    test_database_url: str = "postgresql://localhost:5432/ats_test"
     
     
 
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,25 +1,23 @@
 import pytest
-from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
-
-from app.database.session import get_session
-from app.main import app
 
 from alembic import command
 from alembic.config import Config
+
+from fastapi.testclient import TestClient
+from sqlmodel import Session, create_engine
+
+
+from app.main import app
 from app.config import settings
-
-db_url = "postgresql://postgres:example@localhost:5432/ats_test"
-
+from app.database.session import get_session
 
 
 @pytest.fixture(name="session")
 def session_fixture():
-    engine = create_engine(url=db_url)
+    engine = create_engine(url=settings.test_database_url)
 
     alembic_cfg = Config("alembic.ini")
-    alembic_cfg.set_main_option('sqlalchemy.url', db_url)
+    alembic_cfg.set_main_option("sqlalchemy.url", settings.test_database_url)
 
     # Apply all migrations to set up the database schema
     command.upgrade(alembic_cfg, "head")
@@ -36,7 +34,7 @@ def client_fixture(session: Session):
         return session
 
     app.dependency_overrides[get_session] = get_session_override
-    
+
     client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
